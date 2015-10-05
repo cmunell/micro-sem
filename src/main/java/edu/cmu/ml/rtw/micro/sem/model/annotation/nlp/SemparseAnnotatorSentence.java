2bbaa2790d2d2af23cec0546a748e5d943bd75e0
Bryan Kisiel <bkisiel@cs.cmu.edu>
2015-10-05 11:03:28 -0400

    Ajust timeout based on KBP2015 work

--- src/main/java/edu/cmu/ml/rtw/micro/sem/model/annotation/nlp/SemparseAnnotatorSentence.java
+++ src/main/java/edu/cmu/ml/rtw/micro/sem/model/annotation/nlp/SemparseAnnotatorSentence.java
@@ -25,6 +25,7 @@ import edu.cmu.ml.rtw.generic.data.annotation.nlp.PoSTag;
 import edu.cmu.ml.rtw.generic.data.annotation.nlp.TokenSpan;
 import edu.cmu.ml.rtw.generic.model.annotator.nlp.AnnotatorTokenSpan;
 import edu.cmu.ml.rtw.generic.util.Pair;
+import edu.cmu.ml.rtw.util.Timer;
 import edu.cmu.ml.rtw.generic.util.Triple;
 import edu.cmu.ml.rtw.micro.cat.data.annotation.nlp.AnnotationTypeNLPCat;
 import edu.cmu.ml.rtw.users.jayantk.ccg.CcgParseUtils;
@@ -52,7 +53,13 @@ public class SemparseAnnotatorSentence implements AnnotatorTokenSpan<String> {
 		// Read class parameters.                                                                                                            
 		double[] multitagThresholds = new double[] {0.01, 0.001};
 
-		long maxParseTimeMillis = -1;                                                
+                // bkisiel 2015-09: Some kind of time limit is necessary.  Some quick trials on a
+                // very small test set showed 800ms to be about as low as I could go without recall
+                // suffering noticably.  This seemed to work reasonably well for KBP2015, although
+                // productivity of this microreader is low enough overall, relative to other
+                // microreaders, that one could probably go with a lower threshold without much
+                // affect on the total microreader production.
+		long maxParseTimeMillis = 800;
 		int maxChartSize = Integer.MAX_VALUE;                                            
     
     /*
@@ -102,9 +109,11 @@ public class SemparseAnnotatorSentence implements AnnotatorTokenSpan<String> {
 
 	@Override
 	public List<Triple<TokenSpan, String, Double>> annotate(DocumentNLP document) {
+                Timer t = new Timer();
 		List<Triple<TokenSpan, String, Double>> annotations = Lists.newArrayList();
 		for (int i = 0; i < document.getSentenceCount(); i++) {
 			List<String> tokens = document.getSentenceTokenStrs(i);
+                        if (tokens.size() > 27) continue;  // bkdb: cuts off the great bulk of time-consumption at a cost of maybe 1% to recall
 			List<PoSTag> posTags = document.getSentencePoSTags(i);
 			List<String> pos = Lists.newArrayList();
 			for (PoSTag posTag : posTags) {
@@ -126,11 +135,14 @@ public class SemparseAnnotatorSentence implements AnnotatorTokenSpan<String> {
 				typedMentions.add(new TypedMention(mentionString, categories, startIndex + 1, endIndex));
 			}
 
+                        t.start();
+
 			SupertaggedSentence taggedSentence = ListSupertaggedSentence.createWithUnobservedSupertags(                                                
 					tokens, pos);
 			MentionTaggedSentence mentionSentence = new MentionTaggedSentence(taggedSentence, null, typedMentions);
 
 			CcgParse parse = parser.parse(mentionSentence, null);
+                        int numAnnotations = 0;
 			if (parse != null) {
 				for (SpannedExpression spannedExpression : parse.getSpannedLogicalForms(true)) {
 					Set<MentionRelationInstance> instances = CcgParseUtils.getEntailedRelationInstances(
@@ -142,9 +154,13 @@ public class SemparseAnnotatorSentence implements AnnotatorTokenSpan<String> {
         
 						annotations.add(new Triple<TokenSpan, String, Double>(span,
 								spannedExpression.getExpression().toString(), 1.0));
+                                                numAnnotations++;
 					}
 				}
 			}
+                        if (false)  // For tuning the timeout
+                            System.out.println(tokens.size() + " tokens, " + numAnnotations + " annotations: " + t.getElapsedTimeMilli());
+
 		}
 
 		return annotations;
